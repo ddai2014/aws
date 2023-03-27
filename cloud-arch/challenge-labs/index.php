d16c0973a1a122afe2e952632c6a269c0115af4c
ddai2014 <ddai@cihi.ca>
2023-03-26 21:36:36 -0400

    update index.php

--- cloud-arch/challenge-labs/index.php
+++ cloud-arch/challenge-labs/index.php
@@ -9,16 +9,16 @@
         <h1>David's Demo Site</h1>
     </div>
 <?php
-require 'AWSSDK/aws.phar';
+require 'aws.phar';
 $az = file_get_contents('http://169.254.169.254/latest/meta-data/placement/availability-zone');
 $ipAddress = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
 $instanceID = file_get_contents('http://169.254.169.254/latest/meta-data/instance-id');
 echo '<hr>';
 echo '<div>';
-echo '	<h2>Server Information</h2>';
-echo '	<p>IP Address: ' . $ipAddress . '<br>';
-echo '	   Region/Availability Zone: ' . $ipAddress . '<br>';
-echo '	   Instance ID: ' . $instanceID . '</p>';
+echo '	<h2>Server Information: <br>';
+echo '	    IP Address: ' . $ipAddress . '<br>';
+echo '	   Region/Availability Zone: ' . $az . '<br>';
+echo '	   Instance ID: ' . $instanceID . '</h2>';
 echo '</div>';
 ?>
 </body>
