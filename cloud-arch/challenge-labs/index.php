94da45f8f7c297baa8b5f1c8cb4789d6f8012cc0
ddai2014 <ddai@cihi.ca>
2023-03-30 10:47:59 -0400

    added flag

--- cloud-arch/challenge-labs/index.php
+++ cloud-arch/challenge-labs/index.php
@@ -10,10 +10,15 @@
     </div>
 <?php
 require 'aws.phar';
+$flag = 'https://en.wikipedia.org/wiki/File:Flag_of_Canada_(Pantone).svg';	
 $url = 'http://169.254.169.254/latest/meta-data/';	
 $publicIp = file_get_contents($url . 'public-ipv4');
 $az = file_get_contents($url . 'placement/availability-zone');
 $instanceID = file_get_contents($url . 'instance-id');
+if (str_starts_with($az, 'us') {
+  $flag = 'https://en.wikipedia.org/wiki/File:Flag_of_the_United_States.svg';	
+}
+echo '<div><img src="' .$flag. '" width="200"></div>';	
 echo '<hr>';
 echo '<div>';
 echo '	<h2>Server Information: <br>';
