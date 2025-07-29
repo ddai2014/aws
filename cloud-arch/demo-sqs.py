55b2618105c9d5467e06d428ca0c3c73b2354a9e
ddai2014 <ddai@cihi.ca>
2025-07-29 10:03:27 -0400

    Update demo-sqs.py

--- cloud-arch/demo-sqs.py
+++ cloud-arch/demo-sqs.py
@@ -5,12 +5,9 @@ sqs = boto3.resource('sqs')
 
 # Get the queue by name
 queue = sqs.get_queue_by_name(QueueName = SQS_QUEUE_NAME)
-
-# Process messages from the queue
 print(f"Processing messages from queue: {SQS_QUEUE_NAME}")
 
+# Receive messages from the queue
 for message in queue.receive_messages():
     print(f"Message: {message.body}")
-    message.delete()
-
-    
+    # message.delete()
